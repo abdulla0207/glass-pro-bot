f8aeb8ad57b2fd903c695afa6f74dd9268a677c4
abdulla0207 <abdulla.ermatov0407@gmail.com>
2023-02-07 00:47:44 +0500

    First commit

---
+++ src/main/java/uz/glasspro/UzGlassProBotApplication.java
@@ -0,0 +1,13 @@
+package uz.glasspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzGlassProBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzGlassProBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/glasspro/UzGlassProBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.glasspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzGlassProBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
